9ada84f6fc15cf01e235b234741a19ad74edcd4b
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-07-24 12:54:33 +0300

    Create 88.Merge sorted array.java

---
+++ 88.Merge sorted array/88.Merge sorted array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+            m -= 1;
+            n -= 1;
+            int k = m + n + 1;
+            
+            while(n >= 0){
+                if(m >= 0 && nums1[m] > nums2[n])
+                   nums1[k--] = nums1[m--];
+                else
+                   nums1[k--] = nums2[n--];   
+            }
+            
+            // anthor solution but not efficent
+      
+            // while(n >= 0){
+            //     nums1[k--] = nums2[n--];
+            // }
+
+            // Arrays.sort(nums1);
+    }
+}
+ 
