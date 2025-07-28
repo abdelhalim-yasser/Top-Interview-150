fe2ee9073fc67272cac52697962eed66a82d65db
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-07-28 13:11:37 +0300

    Update Segregate_Even_and_Odd_numbers.java

--- Segregate Even and Odd numbers/Segregate_Even_and_Odd_numbers.java
+++ Segregate Even and Odd numbers/Segregate_Even_and_Odd_numbers.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    void segregateEvenOdd(int arr[]) {
+        Arrays.sort(arr);
+        int[] temp = new int[arr.length];
+        int stop = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % 2 == 0)
+                temp[stop++] = arr[i];
+        }
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % 2 != 0)
+                temp[stop++] = arr[i];
+        }
+        
+        System.arraycopy(temp, 0, arr, 0, arr.length);
+    }
+}
