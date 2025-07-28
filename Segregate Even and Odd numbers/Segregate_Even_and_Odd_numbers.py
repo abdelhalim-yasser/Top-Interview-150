945841d0ebb9adcddd0b394f4e72a4031a63f08a
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-07-28 13:20:21 +0300

    Update Segregate_Even_and_Odd_numbers.py

--- Segregate Even and Odd numbers/Segregate_Even_and_Odd_numbers.py
+++ Segregate Even and Odd numbers/Segregate_Even_and_Odd_numbers.py
@@ -1,1 +1,18 @@
+class Solution:
+    def segregateEvenOdd(self, arr):
+        arr.sort()
+        even = []
+        odd = []
+
+        for num in arr:
+            if num % 2 == 0:
+                even.append(num)
+            else:
+                odd.append(num)
+
+        for i in range(len(arr)):
+            if i < len(even):
+                arr[i] = even[i]
+            else:
+                arr[i] = odd[i - len(even)]
 
